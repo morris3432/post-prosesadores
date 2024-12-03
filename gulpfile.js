c62024bcc1b754b5f58e6dda414cf5766cb89406
Cristian Yancis <bernardoyancis@gmail.com>
2024-12-03 10:12:47 -0600

    nextcss

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,11 @@ const gulp= require('gulp');
 const postcss= require('gulp-postcss');
 const autoprefixer= require('autoprefixer-core');
 const lost = require('lost');
-
+const cssnext= require('gulp-cssnext');
 gulp.task('frontend', (callback)=>{
     var proceso =[lost,autoprefixer];
     return gulp.src('styles.css')
+    .pipe(cssnext({compress:true}))
     .pipe(postcss(proceso))
     .pipe(gulp.dest('./dist'))
     }
